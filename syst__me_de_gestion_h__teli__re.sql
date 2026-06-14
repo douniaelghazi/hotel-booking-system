0eb5b039018d2c21adfe9b72f58bf90604aec58e
douniaelghazi <douniaelghazi02@gmail.com>
2026-06-15 00:21:59 +0100

    Modification

--- syst__me_de_gestion_h__teli__re.sql
+++ syst__me_de_gestion_h__teli__re.sql
@@ -1,31 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1
--- Généré le : ven. 12 juin 2026 à 11:30
--- Version du serveur : 10.4.32-MariaDB
--- Version de PHP : 8.2.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Base de données : `système_de_gestion_hôtelière`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `chambre`
---
 
 CREATE TABLE `chambre` (
   `id_chambre` int(11) NOT NULL,
@@ -36,9 +16,6 @@ CREATE TABLE `chambre` (
   `id_reservation` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Déchargement des données de la table `chambre`
---
 
 INSERT INTO `chambre` (`id_chambre`, `Numero`, `Type`, `Prix`, `Statut`, `id_reservation`) VALUES
 (1, 101, 'Simple', 300.00, 'Occupee', 101),
@@ -49,9 +26,7 @@ INSERT INTO `chambre` (`id_chambre`, `Numero`, `Type`, `Prix`, `Statut`, `id_res
 
 -- --------------------------------------------------------
 
---
--- Structure de la table `client`
---
+
 
 CREATE TABLE `client` (
   `Id_client` int(11) NOT NULL,
@@ -62,9 +37,8 @@ CREATE TABLE `client` (
   `Email` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Déchargement des données de la table `client`
---
+
+
 
 INSERT INTO `client` (`Id_client`, `Nom`, `Prenom`, `Tel`, `Date_naissance`, `Email`) VALUES
 (1, 'ElGhazi', 'Dounia', '0612345678', '20', 'dounia@gmail.com'),
@@ -73,11 +47,6 @@ INSERT INTO `client` (`Id_client`, `Nom`, `Prenom`, `Tel`, `Date_naissance`, `Em
 (4, 'Amrani', 'Omar', '0645678901', '25', 'omar@gmail.com'),
 (5, 'Karimi', 'Salma', '0656789012', '12', 'salma@gmail.com');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `paiment`
---
 
 CREATE TABLE `paiment` (
   `id_paiement` int(11) NOT NULL,
@@ -87,9 +56,7 @@ CREATE TABLE `paiment` (
   `id_reservation` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Déchargement des données de la table `paiment`
---
+
 
 INSERT INTO `paiment` (`id_paiement`, `Montant`, `Date_paiment`, `Mode_paiment`, `id_reservation`) VALUES
 (1, 1500.00, '2026-06-01', 'Carte Bancaire', 101),
@@ -98,11 +65,7 @@ INSERT INTO `paiment` (`id_paiement`, `Montant`, `Date_paiment`, `Mode_paiment`,
 (4, 3500.00, '2026-06-04', 'Virement', 104),
 (5, 1800.00, '2026-06-05', 'Especes', 105);
 
--- --------------------------------------------------------
 
---
--- Structure de la table `réservation`
---
 
 CREATE TABLE `réservation` (
   `id_reservation` int(11) NOT NULL,
@@ -112,9 +75,7 @@ CREATE TABLE `réservation` (
   `Id_client` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Déchargement des données de la table `réservation`
---
+
 
 INSERT INTO `réservation` (`id_reservation`, `date_reservation`, `date_arrivee`, `date_depart`, `Id_client`) VALUES
 (101, '2026-06-01', '2026-06-10', '2026-06-15', 1),
@@ -123,13 +84,7 @@ INSERT INTO `réservation` (`id_reservation`, `date_reservation`, `date_arrivee`
 (104, '2026-06-04', '2026-06-22', '2026-06-28', 4),
 (105, '2026-06-05', '2026-06-25', '2026-06-30', 5);
 
---
--- Index pour les tables déchargées
---
 
---
--- Index pour la table `chambre`
---
 ALTER TABLE `chambre`
   ADD PRIMARY KEY (`id_chambre`),
   ADD KEY `id_reservation` (`id_reservation`);
@@ -177,6 +132,4 @@ ALTER TABLE `réservation`
   ADD CONSTRAINT `réservation_ibfk_1` FOREIGN KEY (`Id_client`) REFERENCES `client` (`Id_client`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
